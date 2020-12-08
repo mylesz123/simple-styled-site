3d62c7b0af45d8310947dc2437960e15475d038e
Myles Young <myles.young@mailchimp.com>
2020-12-08 15:26:58 -0500

    ok this officially deserves to be a component since I put so much damn thought into it

--- src/components/primitives/index.js
+++ src/components/primitives/index.js
@@ -1,4 +1,5 @@
 export { default as Box } from './Box';
+export { default as Button } from './Button';
 export { default as Center } from './Center';
 export { default as Divider } from './Divider';
 export { default as FlexLayout } from './FlexLayout';
